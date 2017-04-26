cbadd1072cf4319bc6f624362b08bafbccd413be
buffasso <vin100.pierre@hotmail.fr>
2017-04-26 19:03:47 +0200

    Add files via upload

---
+++ partenariat.py
@@ -0,0 +1,48 @@
+__author__ = 'desilvsa'
+template=Import('/site_web/template.py')
+
+
+def membres():
+    membres = '''<div class="section mcontainer">
+                    <h2 >Pôle communication</h2>
+                    <div class="row">
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                        <div class="col s12 m3 membre">
+                            <img class="circle img-responsive" src="../assets/imgs/papotto.jpg">
+                            <div class="nom">Lucas Papotto</div>
+                            <div class="titre">Biatch</div>
+                        </div>
+                    </div>
+                </div>'''
+    return membres
+
+def partenariat():
+    result = template.entete('Bureau')
+    result+=template.nav()
+    result+=membres()
+    result+=template.footer()
+    return result
